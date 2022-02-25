f0c29b4603821475759578c9841fa9ccf9d993df
Alex Nunez <nunezaleks@gmail.com>
2022-02-24 16:22:39 -0800

    add header to be rendered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/header/index';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello world</h1>
+        <Header />
       </header>
     </div>
   );
