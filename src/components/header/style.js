ae3951270e213ea5c68e239f1e1b8eb61399bfb8
Alex Nunez <nunezaleks@gmail.com>
2022-02-24 16:22:18 -0800

    add header style file

---
+++ src/components/header/style.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+import { 
+    Brightness6, 
+    GitHub, 
+    Menu, 
+    Print } from '@mui/icons-material'
+
+export const HeaderContainer = styled.div`
+display: flex;
+position: fixed;
+top: 0;
+width: 100%;
+justify-content: space-between;
+align-items: center;
+
+`
+
+export const HeaderLeft = styled.div`
+flex: 0.08;
+
+`
+
+export const HeaderRight = styled.div`
+flex: 0.08;
+display: flex;
+align-items: center;
+
+`
+
+export const HeaderCenter = styled.div`
+align-items: center;
+font-weight: 500;
+ 
+`
+
+export const GitHubIcon = styled(GitHub)`
+transform: scale(0.9);
+cursor: pointer;
+color: #0b4f6c;
+:hover {
+    opacity: 0.7;
+}
+`
+
+export const PrintIcon = styled(Print)`
+padding-right: 10px;
+transform: scale(0.9);
+cursor: pointer;
+color: #0b4f6c;
+:hover {
+    opacity: 0.7;
+}
+`
+
+export const MenuIcon = styled(Menu)`
+padding-right: 10px;
+transform: scale(0.9);
+cursor: pointer;
+color: #0b4f6c;
+:hover {
+    opacity: 0.7;
+}
+`
+
+export const ThemeIcon = styled(Brightness6)`
+transform: scale(0.9);
+cursor: pointer;
+color: #0b4f6c;
+:hover {
+    opacity: 0.7;
+}
+`
+
